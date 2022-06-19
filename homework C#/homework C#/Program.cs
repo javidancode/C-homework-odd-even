8dd5baabbc2f206780ea32d09a6eae226dcd8a74
javidancode <javidanig@code.edu.az>
2022-06-20 03:17:32 +0400

    Added C# homework odd even table

---
+++ homework C#/homework C#/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace homework_introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n = 0;
+            //int m = 100;
+            //int sum = 0;
+
+
+            //for (int i = n; i < m; i++)
+
+            //if (i % 21 == 0)
+            //{
+            //        sum += i;
+            //}
+
+            //Console.WriteLine(sum);
+
+
+            //int n = 2;
+            //int m = 2;
+            //int sum = 0;
+
+            //for (int i = n; i <= m; i+= 2)
+
+            //{
+            //    sum += i;
+            //}
+
+            //Console.WriteLine(n+m);
+
+
+            //int n = 1;
+            //int m = 1;
+            //int sum = 0;
+
+            //for (int i = n; i <= m; i+= 2)
+            //{
+            //    sum += 2;
+
+            //}
+
+            //Console.WriteLine(n+m);
+
+            //int[] numbers = { 22, 13, 24, 25, 11, };
+            //int sum = 0;
+
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+
+            //    if (numbers[i] % 2 == 1)
+            //    {
+            //        sum += numbers[i];
+            //    }
+
+
+
+            //}
+            //Console.WriteLine(sum);
+
+
+            //int[] numbers = { 22, 16, 24, 25, 11, };
+
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        Console.WriteLine(numbers[i]);
+            //    }
+
+
+
+
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
